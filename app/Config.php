8e774627c241d8027f72998846cc94d90b10170d
ivan <gomez.mateos.ivan@iescamas.es>
2021-11-29 14:16:21 +0100

    Commit fichero ivan

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos22021";
+     static public $mvc_bd_usuario  = "myjuanbrav";
+     static public $mvc_bd_clave    = "KM5Svw0v";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
